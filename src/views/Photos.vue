5ee2c13231b8a6fa3d900ad2b57fb56a5ba55d4e
braisv <brais.vidal.rodriguez@gmail.com>
2020-03-17 16:50:47 +0100

    photos update

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
         <h1>Photo Gallery</h1>
+
+        <router-link
+        :to="{ name: 'Photos', params: {id: item} }"
+        v-for="(item, index) of picArr"
+        :key="index"
+         >
+            <button>Pic {{ item }}</button>
+        </router-link>
         <Pic />
+
+        <button @click="home">Home</button>
+        <button @click="back">Back</button>
+        <button @click="next">Next</button>
     </div>
 </template>
 
@@ -12,6 +24,22 @@ export default {
     name: 'Photos',
     components: {
         Pic
+    },
+    data() {
+        return {
+            picArr: [1, 2, 3]
+        }
+    },
+    methods: {
+        home() {
+            this.$router.push('/')
+        },
+        back() {
+            this.$router.go(-1)
+        },
+        next() {
+            this.$router.go(1)
+        }
     }
 }
 </script>
